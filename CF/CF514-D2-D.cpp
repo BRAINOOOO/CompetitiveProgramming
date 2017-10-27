e0809639e517bfb0c3ad73e4c6191ea3f68ff5eb
VAMPIER000001 <30309846+VAMPIER000001@users.noreply.github.com>
2017-10-28 00:20:05 +0200

    Create CF514-D2-D.cpp

---
+++ CF/CF514-D2-D.cpp
@@ -0,0 +1,142 @@
+/*
+the problem's idea is easy but the core of the problem is in the implementation 
+we will use 2 pointers one at the beginning of the subset and one will move to the right
+if the ans will fit else we will move left one to the right tell it fits and we will 
+use multi set to get the max of each detail.
+*/
+#include <bits/stdc++.h>
+#define sz(v)   ((int)(v).size())
+#define  all(v)    ((v).begin()),((v).end())
+#define  allr(v)    ((v).rbegin()),((v).rend())
+#define   pb         push_back
+#define   mp         make_pair
+#define   mt         make_tuple
+#define   inf        0x7fffffff
+#define    clr(v,d)      memset( v, d ,sizeof(v))
+#define    vi       vector<int>
+#define    vd       vector<double>
+#define    vvi      vector< vector<int> >
+#define    vvll      vector< vector<long long> >
+#define    vvpi     vector< vector< pair<int,int> > >
+#define    vvpll     vector< vector< pair<long long,long long> > >
+#define    vvd      vector< vector<double> >
+#define    vs       vector<string>
+#define    vvs      vector< vector<string> >
+#define     vc        vector<char>
+#define     vpi        vector< pair<int ,int> >
+#define      pi        pair<int ,int>
+#define      pll       pair<long long ,long long>
+#define     vpd        vector< pair<double ,double> >
+#define     vpll        vector< pair<long long ,long long> >
+#define    vll       vector<long long>
+#define    dqi        deque< int >
+#define     si         set<int>
+#define     spi        set< pair<int ,int > >
+#define     row          vector< ll >
+#define     matrix            vector< row >
+#define    lp(i,n)    for(i=0;i<n;i++)
+#define     si         set<int>
+#define    oo         100000000000000000
+#define   MOD         1000000007
+#define    X            real
+#define    Y            imag
+#define   angle(n)      atan2((n.imag()),(n.real()))
+#define   vec(a,b)       ((b)-(a))
+#define   length(a)      hypot( (a.imag()),(a.real()) )
+#define   normalize(a)      (a)/(length(a))
+#define    dp(a,b)          (((conj(a))*(b)).real())
+#define    cp(a,b)          (((conj(a))*(b)).imag())
+#define    lengthsqrt(a)       dp(a,a)
+#define    rotate0( a,ang)    ((a)*exp( point(0,ang) ))
+#define    rotateA(about,p,ang)   (rotate0(vec(about,p),ang)+about)
+#define    lcm(a,b)                ((a*b)/(__gcd(a,b)))
+#define    reflection0(m,v)         (conj((v)/(m))*(m))
+#define     reflectionA(m,v,p0)     (conj( (vec(p0,v))/(vec(p0,m)) ) * (vec(p0,m)) ) + p0
+#define     same(p1,p2)               ( dp(  vec(p1,p2),vec(p1,p2)) < eps )
+#define     point                    complex<double>
+#define    scani(n)                  scanf("%d",&n)
+#define    scanll(n)                  scanf("%I64d",&n)
+#define    printi(n)                 printf("%d",n)
+#define    printll(n)                 printf("%I64d",n)
+#define    scand(n)                  scanf("%lf",&n)
+#define    printd(n)                 printf("%lf",n)
+#define    scans(n)                  scanf("%s",&n)
+#define    prints(n)                 printf("%s",n)
+typedef  long long     ll ;
+typedef  unsigned long long ull;
+const double eps= (1e-10);
+const double  PI= acos(-1.0);
+using namespace std;
+int dcmp(double a,double b){   return fabs(a-b)<=eps ? 0: (a>b)? 1:-1  ;}
+int getBit(int num, int idx) {return ((num >> idx) & 1) == 1;}
+int setBit1(int num, int idx) {return num | (1<<idx);}
+int setBit0(int num, int idx) {return num & ~(1<<idx);}
+int flipBit(int num, int idx) {return num ^ (1<<idx);}
+int countNumBit1(int mask) {int ret=0; while (mask) 	{mask &= (mask-1);++ret;	}return ret;}
+int v[100000][5+9],ans[5+9];
+multiset< int > arr[5+9];
+int main()
+{
+    int n,m,k;
+    cin>>n>>m>>k;
+
+    int no;
+
+    for(int i=0;i<n;i++)
+    {
+
+        for(int j=0;j<m;j++)
+        {
+            cin>>no;
+            v[i][j]=no;
+        }
+
+    }
+    int mx=0;
+    for(int l=0,r=0;r<n;r++)
+    {
+
+
+       for(int i=0;i<m;i++)
+       {
+           arr[i].insert(v[r][i]);
+       }
+
+        for(;l<=r;l++)
+        {
+            int sum=0;
+
+            for(int i=0;i<m;i++)
+                sum+=(*arr[i].rbegin());
+
+
+            if(sum<=k)
+                break;
+
+            for(int i=0;i<m;i++)
+           arr[i].erase( arr[i].find(v[l][i]));
+
+
+
+        }
+
+        if(r-l+1>mx)
+        {
+            mx=r-l+1;
+            for(int i=0;i<m;i++)
+                ans[i]=(*arr[i].rbegin());
+        }
+
+
+
+    }
+
+
+   for(int i=0;i<m;i++)
+    cout<<ans[i]<<" ";
+}
+
+
+
+
+
