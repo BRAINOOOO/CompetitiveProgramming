a7f9dc8046585d6c80d55a8c53131da154338f31
VAMPIER000001 <30309846+VAMPIER000001@users.noreply.github.com>
2017-11-11 17:51:31 +0200

    Create CF199-D2-D.cpp

---
+++ CF/CF199-D2-D.cpp
@@ -0,0 +1,116 @@
+/*
+explanation : here the problem is simple dfs on the 2Xn grid to 
+know if there is a path or not .
+
+complexity : dfs complexity
+
+*/
+#include <bits/stdc++.h>
+#define sz(v)   ((int)(v).size())
+#define  all(v)    ((v).begin()),((v).end())
+#define  allr(v)    ((v).rbegin()),((v).rend())
+#define   pb         push_back
+#define   mp         make_pair
+#define   mt         make_tuple
+#define   inf        0x7fffffff
+#define    clr(v,d)      memset( v, d ,sizeof(v))
+#define    vi       vector<int>
+#define    vd       vector<double>
+#define    vvi      vector< vector<int> >
+#define    vvll      vector< vector<long long> >
+#define    vvpi     vector< vector< pair<int,int> > >
+#define    vvpll     vector< vector< pair<long long,long long> > >
+#define    vvd      vector< vector<double> >
+#define    vs       vector<string>
+#define    vvs      vector< vector<string> >
+#define     vc        vector<char>
+#define     vpi        vector< pair<int ,int> >
+#define      pi        pair<int ,int>
+#define      pll       pair<long long ,long long>
+#define     vpd        vector< pair<double ,double> >
+#define     vpll        vector< pair<long long ,long long> >
+#define    vll       vector<long long>
+#define    dqi        deque< int >
+#define     si         set<int>
+#define     spi        set< pair<int ,int > >
+#define     row          vector< ll >
+#define     matrix            vector< row >
+#define    lp(i,n)    for(i=0;i<n;i++)
+#define     si         set<int>
+#define    oo         900000000000000000
+#define   MOD         1000000007
+#define    X            real
+#define    Y            imag
+#define   angle(n)      atan2((n.imag()),(n.real()))
+#define   vec(a,b)       ((b)-(a))
+#define   length(a)      hypot( (a.imag()),(a.real()) )
+#define   normalize(a)      (a)/(length(a))
+#define    dp(a,b)          (((conj(a))*(b)).real())
+#define    cp(a,b)          (((conj(a))*(b)).imag())
+#define    lengthsqrt(a)       dp(a,a)
+#define    rotate0( a,ang)    ((a)*exp( point(0,ang) ))
+#define    rotateA(about,p,ang)   (rotate0(vec(about,p),ang)+about)
+#define    lcm(a,b)                ((a*b)/(__gcd(a,b)))
+#define    reflection0(m,v)         (conj((v)/(m))*(m))
+#define     reflectionA(m,v,p0)     (conj( (vec(p0,v))/(vec(p0,m)) ) * (vec(p0,m)) ) + p0
+#define     same(p1,p2)               ( dp(  vec(p1,p2),vec(p1,p2)) < eps )
+#define     point                    complex<double>
+#define    scani(n)                  scanf("%d",&n)
+#define    scanll(n)                  scanf("%I64d",&n)
+#define    printi(n)                 printf("%d",n)
+#define    printll(n)                 printf("%I64d",n)
+#define    scand(n)                  scanf("%lf",&n)
+#define    printd(n)                 printf("%lf",n)
+#define    scans(n)                  scanf("%s",&n)
+#define    prints(n)                 printf("%s",n)
+typedef  long long     ll ;
+typedef  unsigned long long ull;
+const double eps= (1e-10);
+const double  PI= acos(-1.0);
+using namespace std;
+int dcmp(double a,double b){   return fabs(a-b)<=eps ? 0: (a>b)? 1:-1  ;}
+int getBit(int num, int idx) {return ((num >> idx) & 1) == 1;}
+int setBit1(int num, int idx) {return num | (1<<idx);}
+int setBit0(int num, int idx) {return num & ~(1<<idx);}
+int flipBit(int num, int idx) {return num ^ (1<<idx);}
+int countNumBit1(int mask) {int ret=0; while (mask) 	{mask &= (mask-1);++ret;	}return ret;}
+int vis[2+9][200000+9],n,k,flag;
+char arr[2+9][200000+9];
+void dfs(int r,int c,int w)
+{
+    if(r<1||c<1||r>2||vis[r][c]||c<=w||arr[r][c]=='X')
+        return;
+
+        vis[r][c]=1;
+
+    if(c>n)
+    {
+        flag=1;
+        return;
+    } 
+    dfs(r,c-1,w+1);
+    dfs(r+1,c+k,w+1);
+    dfs(r-1,c+k,w+1);
+    dfs(r,c+1,w+1);
+
+}
+int main()
+{
+
+    cin>>n>>k;
+
+
+    for(int i=1;i<=2;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    flag=0;
+    dfs(1,1,0);
+    if(flag)
+        cout<<"YES\n";
+    else
+        cout<<"NO\n";
+}
