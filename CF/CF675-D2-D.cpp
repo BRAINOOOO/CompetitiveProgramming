cea334c0e505b69d560afe99650960ec1c7f156f
VAMPIER000001 <30309846+VAMPIER000001@users.noreply.github.com>
2017-11-17 03:30:13 +0200

    Create CF675-D2-D.cpp

---
+++ CF/CF675-D2-D.cpp
@@ -0,0 +1,98 @@
+/*
+explaination : the idea is the BST when we traverse it from the leftmost node it is sorted 
+and fill the tree from left to right so when i need to add a node i will add it to left or
+to right of a certain node okay here i fill from left to right so i will search first for the
+the first number greater then me but it can be assigned to a node so i will search for the greatest
+node smaller than me and put it at its right, if you think of constructing the tree and traverse okay
+it can be in log(n) but its worst case is n so its TLE.
+
+complexity : O(n*log(n))
+*/
+#include <bits/stdc++.h>
+#define sz(v)   ((int)(v).size())
+#define  all(v)    ((v).begin()),((v).end())
+#define  allr(v)    ((v).rbegin()),((v).rend())
+#define   pb         push_back
+#define   mp         make_pair
+#define   mt         make_tuple
+#define    clr(v,d)      memset( v, d ,sizeof(v))
+#define    vi       vector<int>
+#define    vd       vector<double>
+#define    vvi      vector< vector<int> >
+#define    vvll      vector< vector<long long> >
+#define    vvpi     vector< vector< pair<int,int> > >
+#define    vvpll     vector< vector< pair<long long,long long> > >
+#define    vvd      vector< vector<double> >
+#define    vs       vector<string>
+#define    vvs      vector< vector<string> >
+#define     vpi        vector< pair<int ,int> >
+#define      pi        pair<int ,int>
+#define      pll       pair<long long ,long long>
+#define     vpd        vector< pair<double ,double> >
+#define     vpll        vector< pair<long long ,long long> >
+#define    vll       vector<long long>
+#define    dqi        deque< int >
+#define     si         set<int>
+#define     spi        set< pair<int ,int > >
+#define     row          vector< ll >
+#define     matrix            vector< row >
+#define     si         set<int>
+#define    oo         900000000000000000
+#define   angle(n)      atan2((n.imag()),(n.real()))
+#define   vec(a,b)       ((b)-(a))
+#define   length(a)      hypot( (a.imag()),(a.real()) )
+#define   normalize(a)      (a)/(length(a))
+#define    dp(a,b)          (((conj(a))*(b)).real())
+#define    cp(a,b)          (((conj(a))*(b)).imag())
+#define    lengthsqrt(a)       dp(a,a)
+#define    rotate0( a,ang)    ((a)*exp( point(0,ang) ))
+#define    rotateA(about,p,ang)   (rotate0(vec(about,p),ang)+about)
+#define    lcm(a,b)                ((a*b)/(__gcd(a,b)))
+#define    reflection0(m,v)         (conj((v)/(m))*(m))
+#define     reflectionA(m,v,p0)     (conj( (vec(p0,v))/(vec(p0,m)) ) * (vec(p0,m)) ) + p0
+#define     same(p1,p2)               ( dp(  vec(p1,p2),vec(p1,p2)) < eps )
+#define     point                    complex<double>
+typedef  long long     ll ;
+typedef  unsigned long long ull;
+const double eps= (1e-10);
+using namespace std;
+int dcmp(double a,double b){   return fabs(a-b)<=eps ? 0: (a>b)? 1:-1  ;}
+int getBit(int num, int idx) {return ((num >> idx) & 1) == 1;}
+int setBit1(int num, int idx) {return num | (1<<idx);}
+int setBit0(int num, int idx) {return num & ~(1<<idx);}
+int flipBit(int num, int idx) {return num ^ (1<<idx);}
+int countNumBit1(int mask) {int ret=0; while (mask) 	{mask &= (mask-1);++ret;	}return ret;}
+map< ll,pll> mb;
+map<ll,ll> par;
+int main()
+{
+    ll n,root;
+    cin>>n>>root;
+   ll no;
+   vll v1;
+   set<ll> s;
+   s.insert(root);
+    for(int i=1;i<n;i++)
+    {
+        cin>>no;
+        v1.pb(no);
+      auto it= s.upper_bound(no);
+     ll num=(*it);
+      if(it!=s.end()&&mb[num].first==0)
+      {
+          mb[num].first=no;
+          par[no]=num;
+          s.insert(no);
+          continue;
+      }
+     auto it1= s.upper_bound(no);
+     it1--;
+      mb[(*it1)].second=no;
+      par[no]=(*it1);
+       s.insert(no);
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        cout<<par[v1[i]]<<" ";
+    }
+}
