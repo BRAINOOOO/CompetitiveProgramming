726291bbf00d6df8fa7736618678ab8f50db5011
VAMPIER000001 <30309846+VAMPIER000001@users.noreply.github.com>
2017-11-20 03:37:38 +0200

    Create UVA 1595.cpp

---
+++ UVA/V-159/UVA 1595.cpp
@@ -0,0 +1,102 @@
+/*
+explaination : here is the problem idea is to get that the vertcal line
+which divides the 2 partions is the orgin between the leftmost and the 
+rightmost dots and that it is the orgin of every point on the left and 
+its apponent in the right .
+
+complexity : O( n^2 )
+*/
+#include <bits/stdc++.h>
+#define sz(v)   ((int)(v).size())
+#define  all(v)    ((v).begin()),((v).end())
+#define  allr(v)    ((v).rbegin()),((v).rend())
+#define   pb         push_back
+#define   mp         make_pair
+#define   mt         make_tuple
+#define    clr(v,d)      memset( v, d ,sizeof(v))
+#define    vi       vector<int>
+#define    vd       vector<double>
+#define    vvi      vector< vector<int> >
+#define    vvll      vector< vector<long long> >
+#define    vvpi     vector< vector< pair<int,int> > >
+#define    vvpll     vector< vector< pair<long long,long long> > >
+#define    vvd      vector< vector<double> >
+#define    vs       vector<string>
+#define    vvs      vector< vector<string> >
+#define     vpi        vector< pair<int ,int> >
+#define      pi        pair<int ,int>
+#define      pll       pair<long long ,long long>
+#define     vpd        vector< pair<double ,double> >
+#define     vpll        vector< pair<long long ,long long> >
+#define    vll       vector<long long>
+#define    dqi        deque< int >
+#define     si         set<int>
+#define     spi        set< pair<int ,int > >
+#define     row          vector< ll >
+#define     matrix            vector< row >
+#define     si         set<int>
+#define    oo         900000000000000000
+#define   angle(n)      atan2((n.imag()),(n.real()))
+#define   vec(a,b)       ((b)-(a))
+#define   length(a)      hypot( (a.imag()),(a.real()) )
+#define   normalize(a)      (a)/(length(a))
+#define    dp(a,b)          (((conj(a))*(b)).real())
+#define    cp(a,b)          (((conj(a))*(b)).imag())
+#define    lengthsqrt(a)       dp(a,a)
+#define    rotate0( a,ang)    ((a)*exp( point(0,ang) ))
+#define    rotateA(about,p,ang)   (rotate0(vec(about,p),ang)+about)
+#define    lcm(a,b)                ((a*b)/(__gcd(a,b)))
+#define    reflection0(m,v)         (conj((v)/(m))*(m))
+#define     reflectionA(m,v,p0)     (conj( (vec(p0,v))/(vec(p0,m)) ) * (vec(p0,m)) ) + p0
+#define     same(p1,p2)               ( dp(  vec(p1,p2),vec(p1,p2)) < eps )
+#define     point                    complex<double>
+typedef  long long     ll ;
+typedef  unsigned long long ull;
+const double eps= (1e-10);
+using namespace std;
+int dcmp(double a,double b){   return fabs(a-b)<=eps ? 0: (a>b)? 1:-1  ;}
+int getBit(int num, int idx) {return ((num >> idx) & 1) == 1;}
+int setBit1(int num, int idx) {return num | (1<<idx);}
+int setBit0(int num, int idx) {return num & ~(1<<idx);}
+int flipBit(int num, int idx) {return num ^ (1<<idx);}
+int countNumBit1(int mask) {int ret=0; while (mask) 	{mask &= (mask-1);++ret;	}return ret;}
+int n,X[1000+9],Y[1000+9],mid;
+bool fun(int i)
+{
+    for(int j=0;j<n;j++)
+        if(Y[j]==Y[i]&&X[i]+X[j]==mid)
+         return true;
+    return false;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        clr(X,0);
+        clr(Y,0);
+        cin>>n;
+        int mx=-10000,mi=10000;
+        for(int i=0;i<n;i++)
+        {
+            cin>>X[i]>>Y[i];
+            mx=max(mx,X[i]);
+            mi=min(mi,X[i]);
+        }
+        mid= (mx+mi);
+        int fl=1;
+        for(int i=0;i<n;i++)
+        {
+          if(!fun(i))
+          {
+            fl=0;
+            break;
+          }
+        }
+        if(fl)
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+}
