96e8d2b28c07f592621274bd687d9274df8040d7
VAMPIER000001 <30309846+VAMPIER000001@users.noreply.github.com>
2017-11-25 04:35:50 +0200

    Create CF58-D2-D.cpp

---
+++ CF/CF58-D2-D.cpp
@@ -0,0 +1,125 @@
+/*
+explaination : here as the number of lines is n/2 and the length of each
+line is equal so the the line length should be equal to sum/(n/2) +1 for 
+the separator know we just searching for the min string+sperator and second
+min to put in the second column and so on.
+
+complexity : O(n^2)
+*/
+#include <bits/stdc++.h>
+#define sz(v)   ((int)(v).size())
+#define  all(v)    ((v).begin()),((v).end())
+#define  allr(v)    ((v).rbegin()),((v).rend())
+#define   pb         push_back
+#define   mp         make_pair
+#define   mt         make_tuple
+#define    clr(v,d)      memset( v, d ,sizeof(v))
+#define    vi       vector<int>
+#define    vd       vector<double>
+#define    vvi      vector< vector<int> >
+#define    vvll      vector< vector<long long> >
+#define    vvpi     vector< vector< pair<int,int> > >
+#define    vvpll     vector< vector< pair<long long,long long> > >
+#define    vvd      vector< vector<double> >
+#define    vs       vector<string>
+#define    vvs      vector< vector<string> >
+#define     vpi        vector< pair<int ,int> >
+#define      pi        pair<int ,int>
+#define      pll       pair<long long ,long long>
+#define     vpd        vector< pair<double ,double> >
+#define     vpll        vector< pair<long long ,long long> >
+#define    vll       vector<long long>
+#define    dqi        deque< int >
+#define     si         set<int>
+#define     spi        set< pair<int ,int > >
+#define     row          vector< ll >
+#define     matrix            vector< row >
+#define     si         set<int>
+#define    oo         900000000000000000
+#define   angle(n)      atan2((n.imag()),(n.real()))
+#define   vec(a,b)       ((b)-(a))
+#define   length(a)      hypot( (a.imag()),(a.real()) )
+#define   normalize(a)      (a)/(length(a))
+#define    dp(a,b)          (((conj(a))*(b)).real())
+#define    cp(a,b)          (((conj(a))*(b)).imag())
+#define    lengthsqrt(a)       dp(a,a)
+#define    rotate0( a,ang)    ((a)*exp( point(0,ang) ))
+#define    rotateA(about,p,ang)   (rotate0(vec(about,p),ang)+about)
+#define    lcm(a,b)                ((a*b)/(__gcd(a,b)))
+#define    reflection0(m,v)         (conj((v)/(m))*(m))
+#define     reflectionA(m,v,p0)     (conj( (vec(p0,v))/(vec(p0,m)) ) * (vec(p0,m)) ) + p0
+#define     same(p1,p2)               ( dp(  vec(p1,p2),vec(p1,p2)) < eps )
+#define     point                    complex<double>
+typedef  long long     ll ;
+typedef  unsigned long long ull;
+const double eps= (1e-10);
+using namespace std;
+int dcmp(double a,double b){   return fabs(a-b)<=eps ? 0: (a>b)? 1:-1  ;}
+int getBit(int num, int idx) {return ((num >> idx) & 1) == 1;}
+int setBit1(int num, int idx) {return num | (1<<idx);}
+int setBit0(int num, int idx) {return num & ~(1<<idx);}
+int flipBit(int num, int idx) {return num ^ (1<<idx);}
+int countNumBit1(int mask) {int ret=0; while (mask) 	{mask &= (mask-1);++ret;	}return ret;}
+int vis[10000+9];
+int main()
+{
+    int n;
+    cin>>n;
+    string s;
+    vs v;
+    int siz=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>s;
+        siz+=sz(s);
+
+        v.pb(s);
+    }
+    string sp;
+    cin>>sp;
+   int p= siz/(n/2)+1;
+
+   for(int i=0;i<n;i++)
+    v[i]=v[i]+sp;
+
+    sort(all(v));
+    vs v1,v2,v3;
+
+    for(int i=0;i<n;i++)
+    {
+        if(vis[i])
+            continue;
+
+        vis[i]=1;
+       int need=p-sz(v[i]);
+        v1.pb(v[i]);
+        for(int j=0;j<n;j++)
+        {
+            if(!vis[j]&&sz(v[j])==need+1)
+            {
+                v2.pb(v[j]);
+                vis[j]=1;
+                break;
+            }
+        }
+    }
+    for(int i=0;i<n/2;i++)
+    {
+        v2[i].pop_back();
+        string nw= v1[i]+v2[i];
+        v3.pb(nw);
+    }
+    for(int i=0;i<n/2;i++)
+        cout<<v3[i]<<endl;
+}
+/*
+Cases
+
+4
+aa
+aaa
+a
+aaaa
+b
+
+*/
