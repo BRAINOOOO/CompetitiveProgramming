b98e2d3244518a385d3b41fbb9506f9cb768446d
VAMPIER000001 <30309846+VAMPIER000001@users.noreply.github.com>
2017-12-03 07:53:17 +0200

    Create UVA 11021.cpp

---
+++ UVA/V-110/UVA 11021.cpp
@@ -0,0 +1,91 @@
+/*
+explanation : here first you must notice that the probabilty of k tribbles to be zero 
+at level m is indepentant so we can work as k=1 then off course the probability of the
+the k tribbles at first are the same in acheiving zero children at the m level so the 
+next sub problem is if k1 how we could calculate the probability if we the intial state
+is if m=1 its only p0 but if m=2 we will branch at first the ends with the same as when 
+m=1, what i want to say is we will build the tree from down to up.
+
+complexity : O( t*m*n )
+*/
+#include <bits/stdc++.h>
+#define sz(v)   ((int)(v).size())
+#define  all(v)    ((v).begin()),((v).end())
+#define  allr(v)    ((v).rbegin()),((v).rend())
+#define   pb         push_back
+#define   mp         make_pair
+#define   mt         make_tuple
+#define    clr(v,d)      memset( v, d ,sizeof(v))
+#define    vi       vector<int>
+#define    vd       vector<double>
+#define    vvi      vector< vector<int> >
+#define    vvll      vector< vector<long long> >
+#define    vvpi     vector< vector< pair<int,int> > >
+#define    vvpll     vector< vector< pair<long long,long long> > >
+#define    vvd      vector< vector<double> >
+#define    vs       vector<string>
+#define    vvs      vector< vector<string> >
+#define     vpi        vector< pair<int ,int> >
+#define      pi        pair<int ,int>
+#define      pll       pair<long long ,long long>
+#define     vpd        vector< pair<double ,double> >
+#define     vpll        vector< pair<long long ,long long> >
+#define    vll       vector<long long>
+#define    dqi        deque< int >
+#define     si         set<int>
+#define     spi        set< pair<int ,int > >
+#define     row          vector< ll >
+#define     matrix            vector< row >
+#define     si         set<int>
+#define    oo         900000000000000000
+#define   angle(n)      atan2((n.imag()),(n.real()))
+#define   vec(a,b)       ((b)-(a))
+#define   length(a)      hypot( (a.imag()),(a.real()) )
+#define   normalize(a)      (a)/(length(a))
+#define    dp(a,b)          (((conj(a))*(b)).real())
+#define    cp(a,b)          (((conj(a))*(b)).imag())
+#define    lengthsqrt(a)       dp(a,a)
+#define    rotate0( a,ang)    ((a)*exp( point(0,ang) ))
+#define    rotateA(about,p,ang)   (rotate0(vec(about,p),ang)+about)
+#define    lcm(a,b)                ((a*b)/(__gcd(a,b)))
+#define    reflection0(m,v)         (conj((v)/(m))*(m))
+#define     reflectionA(m,v,p0)     (conj( (vec(p0,v))/(vec(p0,m)) ) * (vec(p0,m)) ) + p0
+#define     same(p1,p2)               ( dp(  vec(p1,p2),vec(p1,p2)) < eps )
+#define     point                    complex<double>
+typedef  long long     ll ;
+typedef  unsigned long long ull;
+const double eps= (1e-10);
+using namespace std;
+int dcmp(double a,double b){   return fabs(a-b)<=eps ? 0: (a>b)? 1:-1  ;}
+int getBit(int num, int idx) {return ((num >> idx) & 1) == 1;}
+int setBit1(int num, int idx) {return num | (1<<idx);}
+int setBit0(int num, int idx) {return num & ~(1<<idx);}
+int flipBit(int num, int idx) {return num ^ (1<<idx);}
+int countNumBit1(int mask) {int ret=0; while (mask) 	{mask &= (mask-1);++ret;	}return ret;}
+double f[1000+9];
+int main()
+{
+    int t;
+    cin>>t;
+    int l=1;
+    while(t--)
+    {
+        int n,k,m;
+        cin>>n>>k>>m;
+        double no;
+        vd p;
+        for(int i=0;i<n;i++)
+            cin>>no,p.pb(no);
+        clr(f,0);
+        f[1]=p[0];
+        for(int i=2;i<=m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                f[i]+= p[j] * pow(f[i-1],j);
+
+            }
+        }       
+        printf("Case #%d: %.7f\n",l++,pow(f[m],k));
+    }
+}
