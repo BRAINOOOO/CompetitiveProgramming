31c3d2d25e20b1c77fbda9374580640da2685f29
VAMPIER000001 <30309846+VAMPIER000001@users.noreply.github.com>
2017-12-08 17:23:16 +0200

    Create UVA 11628.cpp

---
+++ UVA/V-116/UVA 11628.cpp
@@ -0,0 +1,66 @@
+/*
+topic : mainly greedy as it deals with basic probability
+
+explanation : it is easy to obseve that it doesnt matter want you gain before the last round
+because the one who gain the last round will have the greatest money since if there is another 
+one win all the rounds except for the last round will have 2^n-1 money and the one who only win
+the last round will have 2^n so it is greater.
+
+comlexity : O( n*m )
+*/
+#include <bits/stdc++.h>
+#define sz(v)   ((int)(v).size())
+#define  all(v)    ((v).begin()),((v).end())
+#define  allr(v)    ((v).rbegin()),((v).rend())
+#define   pb         push_back
+#define   mp         make_pair
+#define   mt         make_tuple
+#define    clr(v,d)      memset( v, d ,sizeof(v))
+#define   angle(n)      atan2((n.imag()),(n.real()))
+#define   vec(a,b)       ((b)-(a))
+#define   length(a)      hypot( (a.imag()),(a.real()) )
+#define   normalize(a)      (a)/(length(a))
+#define    dp(a,b)          (((conj(a))*(b)).real())
+#define    cp(a,b)          (((conj(a))*(b)).imag())
+#define    lengthsqrt(a)       dp(a,a)
+#define    rotate0( a,ang)    ((a)*exp( point(0,ang) ))
+#define    rotateA(about,p,ang)   (rotate0(vec(about,p),ang)+about)
+#define    lcm(a,b)                ((a*b)/(__gcd(a,b)))
+#define    reflection0(m,v)         (conj((v)/(m))*(m))
+#define     reflectionA(m,v,p0)     (conj( (vec(p0,v))/(vec(p0,m)) ) * (vec(p0,m)) ) + p0
+#define     same(p1,p2)               ( dp(  vec(p1,p2),vec(p1,p2)) < eps )
+#define     point                    complex<double>
+typedef  long long     ll ;
+typedef  unsigned long long ull;
+const double eps= (1e-10);
+using namespace std;
+int dcmp(double a,double b){   return fabs(a-b)<=eps ? 0: (a>b)? 1:-1  ;}
+int getBit(int num, int idx) {return ((num >> idx) & 1) == 1;}
+int setBit1(int num, int idx) {return num | (1<<idx);}
+int setBit0(int num, int idx) {return num & ~(1<<idx);}
+int flipBit(int num, int idx) {return num ^ (1<<idx);}
+int countNumBit1(int mask) {int ret=0; while (mask) 	{mask &= (mask-1);++ret;	}return ret;}
+int main()
+{
+    int n,m;
+   while( cin>>n>>m)
+   {
+       if(n==0&&m==0)
+        break;
+     int arr[10000+9];
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>arr[i];
+        }
+        sum+=arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+       int g= __gcd(arr[i],sum);
+        cout<< arr[i]/g <<" / " <<sum/g <<endl;
+    }
+   }
+}
